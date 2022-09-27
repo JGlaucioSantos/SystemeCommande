f323dd77d9cd43c4f8bb48dae590990bcb830a4f
GLaucio Santos <jgs45600@gmail.com>
2022-09-27 12:22:01 -0400

    test premiere class Categorie

--- src/main/java/com/glaucio/santos/domain/Categorie.java
+++ src/main/java/com/glaucio/santos/domain/Categorie.java
@@ -12,31 +12,42 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categorie implements Serializable {
+public class Categorie implements Serializable { // Serializable é o processo no qual a instancia de um objeto é transformada em uma sequencia de bytes
+												 // é utilizada quando precisamos enviar objetos pela rede, salvar no disco ou comunicar de uma JVM com outra
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	private String nom;
+	private Integer id; // atributos basicos
+	private String nom; // atributos basicos
+	
+	// classe prvada vc coloca os atributos nesta classes, porque ele nao sao acessiveis por outras clases
 	
 	@ManyToMany(mappedBy="categories")
 	private List<Produit> produits = new ArrayList<>();
 	
 	public Categorie () {
+		
+		// construtor vazio, significa instaciar os objetos sem jogar nada para os atributos.
 	}
 
 	public Categorie(Integer id, String nom) {
+		
+		// Criacao do construtor com os atributos que vai receber  o id e o nome para povoar os dados
+		// caminho automatico para criacao do construtor = botao direito + Source + Generate Constructor using Fields
 		super();
 		this.id = id;
 		this.nom = nom;
 	}
+	
+	// Criacao dos gets e sets que sao metodos de acesso para os atributos (id e nome) em virtude de ter uma classe privada
+	// caminho automatico para criacao dos gets e sets = botao direito + Source + Generate Getters e Setters
 
-	public Integer getId() {
+	public Integer getId() {  // get é um metod para obter o valor do atributo (id retorna o id)
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Integer id) { // set é o metodo especial utilizado para alterar o valor do atributo (id recebendo o id atribui o id para o campo)
 		this.id = id;
 	}
 
@@ -57,7 +68,8 @@ public class Categorie implements Serializable {
 	}
 	
 	@Override
-	public int hashCode() {
+	public int hashCode() { // hash code icons sao operacoes para comparar os objetos por valor
+		// caminho automatico para criacao dos gets e sets = botao direito + Source + Generate hashCode() and equals()
 		return Objects.hash(id);
 	}
 
@@ -72,6 +84,12 @@ public class Categorie implements Serializable {
 		Categorie other = (Categorie) obj;
 		return Objects.equals(id, other.id);
 	}
-
+	
 }
+
+// Encapsuliamento é quando transformamos um procedimento que apresenta muitas regras de negocio ou rotinas complxas em um metodo.
+
+
+
+
 
--- src/main/java/com/glaucio/santos/resourses/CategorieResourses.java
+++ src/main/java/com/glaucio/santos/resourses/CategorieResourses.java
@@ -1,5 +1,8 @@
 package com.glaucio.santos.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +14,20 @@ import com.glaucio.santos.domain.Categorie;
 import com.glaucio.santos.services.CategorieService;
 
 @RestController // Responsavel por controlar as requisicoes indicando quem deve receber as aquisicoes para quem deve responde-las 
-@RequestMapping(value = "/Categories") // E a anotacao utilizada tradicionalmente para implementar URL handler e suporta metodos Post, Get, Put, Delete e Pach
+@RequestMapping(value = "/categories") // E a anotacao utilizada tradicionalmente para implementar URL handler e suporta metodos Post, Get, Put, Delete e Pach
 public class CategorieResourses {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String liste() {
-		return  "REST OK";
+	public List<Categorie> liste() { // testando o rest localhost:8081/categories
+		
+		Categorie cat1 = new Categorie (1, "Informatique");
+		Categorie cat2 = new Categorie (2, "Bureau");
+		
+		List<Categorie> listar = new ArrayList<>();
+		listar.add(cat1);
+		listar.add(cat2);
+		
+		return  listar;
 	}
 	
 	@Autowired
